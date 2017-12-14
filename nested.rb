ff0bb73071c1599854dd1decbacc0510c2d87a32
Vaibhav Patel <vp932712@gmail.com>
2017-12-14 21:10:37 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -124,7 +124,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:"dennis_ritchie"][:"languages"] = arr 
+programmer_hash[:"dennis_ritchie"][:"languages"] = arr
 arr.push("Assembly")
+programmer_hash
 
 end
